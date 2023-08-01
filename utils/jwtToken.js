2da397403cf60056a38c13171438638e40c5f10f
Atul Patel <75057837+atulpatel-03@users.noreply.github.com>
2023-08-01 16:48:55 +0530

    Update cookie saving technique in jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,21 +1,24 @@
 // Create Token and saving in cookie
 
-const sendToken = (user, statusCode, res) => {
-  const token = user.getJWTToken();
+const sendToken = async (user, statusCode, res) => {
+  const token = await user.getJWTToken()
 
+  
   // options for cookie
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  };
+  }
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.cookie("token", token, options)
+
+  res.status(statusCode).json({
     success: true,
     user,
     token,
-  });
+  })
 };
 
 module.exports = sendToken;
